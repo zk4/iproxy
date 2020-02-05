d4e3e2c65b592f54d0e10f8b2252eadfbba1caa4
zk <liuzq7@gmail.com>
2020-02-06 00:58:12 +0800

    add recycle urls

--- iproxy/iproxy.py
+++ iproxy/iproxy.py
@@ -6,47 +6,61 @@ from .logx.color import color
 from .get_candidates import main as get_candidates
 import time
 import sys
+import os
 import threading
 import argparse
 import logging
 setup_logging()
 logger = logging.getLogger(__name__)
-backup_urls= set()
+
+# for use
+good_urls= set()
+
+google_ok_urls= set()
+
+# saved all history_urls 
+history_urls = set() 
 
 def check(proxyUrl,targetUrl="http://www.google.com"):
     proxyUrl = proxyUrl.strip()
-    logger.debug(f'try ... {proxyUrl}')
+    logger.info(f'check {proxyUrl}')
     proxyUrl = proxyUrl.strip()
     proxies = { "https":proxyUrl ,"http": proxyUrl}
 
     r = requests.get(targetUrl,proxies = proxies,timeout=5)
     if r.status_code == 200:
-        logger.info("google ok: %s" % proxyUrl)
-        backup_urls.add(proxyUrl)
+        logger.debug("google ok: %s" % proxyUrl)
+        google_ok_urls.add(proxyUrl)
         return True
     return False
 
-rlock = threading.RLock()
 def backup(proxyUrl):
-    with rlock:
-        with open("./backup_goods.txt",'') as f:
+    with open("./good_urls.txt",'w') as f:
+        for line in good_urls:
+            f.write(line+"\n")
+    with open("./google_ok_urls.txt",'w') as f:
+        for line in google_ok_urls:
+            f.write(line+"\n")
+
+
+
+def candidate(filename):
+    if os.path.exists(filename):
+        with open(filename) as f:
             for line in f:
                 yield line
 
+def candidates(all_candidates=False):
+    yield from candidate("./google_ok_urls.txt")
+    yield from candidate("./good_urls.txt")
+    yield from candidate("./candidates.txt")
+    if all_candidates:
+        yield from candidate("./history_urls.txt")
 
-
-def write(localfile,line):
-    with open(localfile,"+a") as f:
-        f.write(line+"\n")
-
-def lines(localfile):
-    with open(localfile) as f:
-        for line in f:
-            yield line
 
 def speedTest(proxyUrl,url) :
     proxyUrl = proxyUrl.strip()
-    logger.debug(f'speed test... {proxyUrl}           ')
+    logger.debug(f'speed: {proxyUrl}          ')
     start = time.time()
     proxies = { "https":proxyUrl ,"http": proxyUrl}
     r = requests.get(url,proxies=proxies, stream=True)
@@ -57,30 +71,29 @@ def speedTest(proxyUrl,url) :
     else:
         total_length = int(total_length)
         if total_length  != 104857600:
-             logger.debug(f'content length is too small')
+             logger.debug(f'content length is too small {proxyUrl}')
              return 
         logger.debug(f'content lenght is {total_length}')
-        lowerSpeedTimesMax = 800
-        lowerSpeedLimit = 50
+        lowerSpeedTimesMax = 100
+        lowerSpeedLimit = 10
         lowerSpeedTimes = 0
-        testLengthPercentage = 0.005
+        testLengthPercentage = 0.01
         
         for chunk in r.iter_content(1024):
             dl += len(chunk)
-            done = int(50 * dl / total_length)
             if (dl / total_length) > testLengthPercentage:
                 break
             speed = dl/1024//(time.time() - start)
-            logger.debug("[%s%s] %s k/bps" % ('=' * done, ' ' * (50-done),speed ))
             logger.debug("%s: %s kb/s" % (proxyUrl,speed))
             if speed < lowerSpeedLimit:
                 lowerSpeedTimes+=1
                 if lowerSpeedTimes > lowerSpeedTimesMax:
-                    logger.debug(f'too slow {proxyUrl}')
+                    logger.debug(f'slow {speed} kb/s {proxyUrl}')
                     return
             else:
                 lowerSpeedTimes=0
         logger.info(f'good! {speed} kb/s {proxyUrl}')
+        good_urls.add(proxyUrl)
 
 
 
@@ -105,9 +118,18 @@ def main(args):
         return
 
     if args.check_file:
+        no_duplicates = set()
         with ThreadPoolExecutor(max_workers=10) as executor:
-            for proxyUrl in lines(args.check_file):
-                executor.submit(combine,proxyUrl)
+            for proxyUrl in candidates(args.all_candidates):
+                if proxyUrl not in no_duplicates:
+                    history_urls.add(proxyUrl)
+                    no_duplicates.add(proxyUrl)
+                    executor.submit(combine,proxyUrl)
+
+        executor.shutdown()
+        backup("./good_urls.txt")
+        print("end ..")
+
 
 
 
@@ -120,9 +142,8 @@ def entry_point():
 def createParse():
     print(color.R+"WARNNING:Proxy is checked for certification validation. Don`t use public proxy to access private data when SSL invalidated! You may under MITM attack if so!"+color.W)
     parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="")
-    # subparsers = parser.add_subparsers()
-    # check_parser = subparsers.add_parser('check',formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="",  help='check proxy connection from your pc')
     parser.add_argument('-c', '--check_file',type=str,required=False, help='local candidates file, content format schema://ip:port', default="./candidates.txt")  
     parser.add_argument('-g', '--get_candidates', action='store_true', help='candidates web sites, content format schema://ip:port')  
+    parser.add_argument('-a', '--all_candidates', action='store_true',default=False, help='if test all candidates in history_urls.txt')  
 
     return parser
